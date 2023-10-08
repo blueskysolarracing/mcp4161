1ecb4ca3ccce92b7e3cc9bbe4f63f617f5907eae
Juho Kim <juho-kim@outlook.com>
2023-10-07 23:41:43 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,14 +2,12 @@
 
 from setuptools import find_packages, setup
 
-with open('README.rst', 'r') as file:
-    long_description = file.read()
-
 setup(
     name='mcp4161',
     version='0.0.0.dev6',
-    description='A Python driver for Microchip Technology MCP4161 7/8-Bit Single/Dual SPI Digital POT with Non-Volatile Memory',
-    long_description=long_description,
+    description='A Python driver for Microchip Technology MCP4161 7/8-Bit '
+                'Single/Dual SPI Digital POT with Non-Volatile Memory',
+    long_description=open('README.rst').read(),
     long_description_content_type='text/x-rst',
     url='https://github.com/blueskysolarracing/mcp4161',
     author='Blue Sky Solar Racing',
@@ -29,18 +27,14 @@ setup(
         'Programming Language :: Python :: 3.11',
         'Programming Language :: Python :: 3.12',
     ],
-    keywords=[
-        'python',
-    ],
+    keywords=['python', 'mcp4161', 'microchip technology'],
     project_urls={
         'Documentation': 'https://mcp4161.readthedocs.io/en/latest/',
         'Source': 'https://github.com/blueskysolarracing/mcp4161',
         'Tracker': 'https://github.com/blueskysolarracing/mcp4161/issues',
     },
     packages=find_packages(),
-    install_requires=[
-         'python-periphery>=2.4.1,<3',
-    ],
+    install_requires=['python-periphery>=2.4.0,<3'],
     python_requires='>=3.11',
     package_data={'mcp4161': ['py.typed']},
 )
